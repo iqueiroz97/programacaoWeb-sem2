273535025c6165bd44622ea1e8240966e6f9ef00
Igor Queiroz <60364183+iqueiroz97@users.noreply.github.com>
2025-03-13 22:04:14 -0300

    Inicio ADO2

---
+++ script.js
@@ -0,0 +1,13 @@
+var usuario = `admin`;
+var senha = `1234`;
+
+function login() {
+    var usuario = document.getElementById(`txtUsuario`).value;
+    var senha = document.getElementById(`txtSenha`).value;
+
+    if (txtUsuario == ) {
+        
+    } else {
+        
+    }
+}
